befdd4744c5c32b1a6630c2c8a25bc3f3b5ca797
Will Haarhoff <williamhaarhoff@gmail.com>
2026-03-26 22:31:17 +1300

    Failing to assemble
    Signed-off-by: Will Haarhoff <williamhaarhoff@gmail.com>

--- fw/memory.x
+++ fw/memory.x
@@ -1,7 +1,6 @@
 MEMORY
 {
   /* NOTE 1 K = 1 KiBi = 1024 bytes */
-  /* These values correspond to the NRF52840 with Softdevices S140 7.0.1 */
-  FLASH : ORIGIN = 0x8020000, LENGTH = 384K
-  RAM : ORIGIN = 0x20000000, LENGTH = 128K
+  FLASH : ORIGIN = 0x8000000, LENGTH = 64K
+  RAM : ORIGIN = 0x20000000, LENGTH = 20K
 }
---
+++ fw/src/main.rs
@@ -0,0 +1,35 @@
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use defmt_rtt as _; // <- RTT logging
+                    // use defmt_serial as _;
+                    // use defmt_panic as _;
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::{Duration, Timer};
+use panic_halt as _;
+use panic_probe as _;
+
+//#[defmt::panic_handler]
+//fn panic() -> ! {
+//    cortex_m::asm::udf()
+//}
+//
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    info!("🔌 Hello from Embassy STM32!");
+    let p = embassy_stm32::init(Default::default());
+    let mut led = Output::new(p.PA6, Level::Low, Speed::Low);
+
+    loop {
+        info!("🔆 LED on");
+        led.set_high();
+        Timer::after(Duration::from_millis(500)).await;
+
+        info!("🌑 LED off");
+        led.set_low();
+        Timer::after(Duration::from_millis(500)).await;
+    }
+}
